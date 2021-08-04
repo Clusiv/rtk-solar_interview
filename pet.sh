e9e4909f25d39d4358b4626fdb8c48a0ab53bd3e
Imran Uziev <sys_adm95@inbox.ru>
2021-08-04 11:31:01 +0300

    Pet task

---
+++ pet.sh
@@ -0,0 +1,23 @@
+petsnames=("Jack_20210321" "Cat_20210321" "Pet_20210321" "Tom_20210321" )
+pets=()
+
+for pet in ${petsnames[@]}
+do  
+    echo "\n" $pet 
+    js=( $(curl -d '{"name":"'$pet'"}' -H "Content-Type: application/json" -X POST https://petstore.swagger.io/v2/pet | awk -F'[:",]' '{print $4}') )
+    # if  "{$js: -1}" in 7 8 9 then
+    #     pets+=$pet:NULL
+    # else
+    #     pets+=$pet:$js
+    # fi
+    echo "${js: -1}"
+    case "${js: -1}" in
+    7|8|9) 
+        pets+=$pet:NULL
+        response=$(curl -X DELETE "https://petstore.swagger.io/v2/pet/${js}")
+        # echo $response
+        ;;
+    *)     pets+=$pet:$js;;
+    esac
+done
+echo $pets
